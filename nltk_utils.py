d48b06a55852781947fff7f5bbc472e78a6ba911
pritamkandula <67675851+pritamkandula@users.noreply.github.com>
2022-07-27 11:44:46 +0530

    Update nltk_utils.py

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,6 +1,7 @@
 import nltk
 import numpy as np
 #nltk.download('punkt')
+# 'punkt' is a tokenizer that divides the text into smaller elements by using an unsupervised algorithm
 from nltk.stem.porter import PorterStemmer
 
 stemmer = PorterStemmer()
@@ -17,8 +18,10 @@ def bag_of_words(tokenized_sentence, all_words):
     # words = ['hi','how','hello','you','ok']
     # bag = [0, 1, 0, 1, 0]
      
+    #stemming of each word is performed
     sentence_words = [stem(w) for w in tokenized_sentence]
 
+    #initialize the bag with zeroes for every word
     bag = np.zeros(len(all_words), dtype = np.float32)
     for idx,w in enumerate(all_words):
         if w in sentence_words:
